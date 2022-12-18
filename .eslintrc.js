67db03b4037967c324ba7207f4c36f5555e5319b
Jinwoo Lee <binarywooo@gmail.com>
2022-12-19 05:15:05 +0900

    chore: set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+};
